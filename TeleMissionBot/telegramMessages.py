4292adc3a3b8ff7111c216934071d0fd41806206
NitionVR <kmaganyane83@gmail.com>
2023-12-23 07:53:46 +0200

    created config_file(): reads config,ini, create configure_api(config): returns api_id, api_hash, created configure_user(config): returns phone_number,username, created create_client(): creates and returns telegram client

--- TeleMissionBot/telegramMessages.py
+++ TeleMissionBot/telegramMessages.py
@@ -9,16 +9,29 @@ from telethon.tl.functions.messages import (GetHistoryRequest)
 from telethon.tl.types import (PeerChannel)
 
 
-#Read config file
-config = configparser.ConfigParser()
-config.read("config.ini")
+def config_file():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
 
-#Getting and Setting Api configuration values
-api_id = config["Telegram"]["api_id"]
-api_hash = str(config["Telegram"]["api_hash"])
 
-#Getting and Setting telegram account details
-phone_number = config["Telegram"]["phone_number"]
-username = config["Telegram"]["username"]
+def configure_api(config):
+    api_id = config["Telegram"]["api_id"]
+    api_hash = str(config["Telegram"]["api_hash"])
+    return (api_id,api_hash)
 
+
+def configure_user(config):
+    phone_number = config["Telegram"]["phone_number"]
+    username = config["Telegram"]["username"]
+    return (phone_number,username)
+
+
+def create_client():
+    config = config_file()
+    api_id,api_hash = configure_api(config)
+    username = configure_user(config)[1]
+
+    client = TelegramClient(username,api_id,api_hash)
+    return client
 
