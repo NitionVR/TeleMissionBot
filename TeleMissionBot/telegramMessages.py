ffdc2a761f2abfd2fd2a3fe678200fc3cd226177
NitionVR <kmaganyane83@gmail.com>
2023-12-23 08:41:21 +0200

    added an asynchronous get_messages(client,my_channel) gets all messages

--- TeleMissionBot/telegramMessages.py
+++ TeleMissionBot/telegramMessages.py
@@ -61,3 +61,34 @@ def get_channel_info():
     return entity
     
 
+async def get_messages(client,my_channel):
+    offset_id = 0
+    limit = 100
+    all_messages = []
+    total_messages = 0
+    total_count_limit = 0
+
+    while True:
+        print(f"Current Offset ID is: {offset_id}; Total Messages: {total_messages}")
+        history = await client(GetHistoryRequest(
+            peer = my_channel,
+            offset_id = offset_id,
+            offset_date = None,
+            add_offset = 0,
+            limit = limit,
+            max_id = 0,
+            min_id = 0,
+            hash = 0,
+        ))
+
+        if not history.messages:
+            break
+        messages = history.messages
+        for message in messages:
+            all_messages.append(message.to_dict())
+            offset_id = messages[len(messages)-1].id
+            total_messages = len(all_messages)
+            if total_count_limit != 0 and total_messages >= total_count_limit:
+                break
+
+                                 
