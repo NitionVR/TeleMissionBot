9a83e02036c2d508a68ee01b1267ce84f4bfc009
NitionVR <kmaganyane83@gmail.com>
2023-12-23 07:06:02 +0200

    added git ignore, moved planning

--- TeleMissionBot/telegramMessages.py
+++ TeleMissionBot/telegramMessages.py
@@ -1,2 +1,10 @@
+import asyncio
+from datetime import date, datetime, timedelta
+
 from telethon import TelegramClient, events, sync
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (PeerChannel)
+
+
 
