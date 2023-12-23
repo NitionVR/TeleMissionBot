edd6bb0b799fce814e6c35ad690fbb9f6b49001a
NitionVR <kmaganyane83@gmail.com>
2023-12-23 07:16:49 +0200

    Reading config file

--- TeleMissionBot/telegramMessages.py
+++ TeleMissionBot/telegramMessages.py
@@ -1,3 +1,5 @@
+import configparser
+import json
 import asyncio
 from datetime import date, datetime, timedelta
 
@@ -6,5 +8,9 @@ from telethon.errors import SessionPasswordNeededError
 from telethon.tl.functions.messages import (GetHistoryRequest)
 from telethon.tl.types import (PeerChannel)
 
+
+#Read config file
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 
