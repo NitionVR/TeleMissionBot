2cdcbb96067348c77c33382ed8a2080c8e1a1abf
NitionVR <kmaganyane83@gmail.com>
2023-12-23 14:57:19 +0200

    Broke down the get_messages function into small functions: fetch_messages(params), process_messages(params) and get_messages(params)

--- TeleMissionBot/telegramMessages.py
+++ TeleMissionBot/telegramMessages.py
@@ -87,8 +87,34 @@ async def get_channel():
             return my_channel
         except ValueError:
             continue
-        
-async def get_messages(client,my_channel):
+
+async def fetch_messages(client, my_channel, offset_id, limit):
+    return await client(GetHistoryRequest(
+        peer=my_channel,
+        offset_id=offset_id,
+        offset_date=None,
+        add_offset=0,
+        limit=limit,
+        max_id=0,
+        min_id=0,
+        hash=0,
+    ))
+
+async def process_messages(messages, total_messages, total_count_limit, all_messages):
+    if not messages:
+        return False
+
+    for message in messages:
+        all_messages.append(message.to_dict())
+        offset_id = messages[-1].id
+        total_messages = len(all_messages)
+
+        if total_count_limit != 0 and total_messages >= total_count_limit:
+            return False
+
+    return True
+
+async def get_messages(client, my_channel):
     offset_id = 0
     limit = 100
     all_messages = []
@@ -97,30 +123,14 @@ async def get_messages(client,my_channel):
 
     while True:
         print(f"Current Offset ID is: {offset_id}; Total Messages: {total_messages}")
-        history = await client(GetHistoryRequest(
-            peer = my_channel,
-            offset_id = offset_id,
-            offset_date = None,
-            add_offset = 0,
-            limit = limit,
-            max_id = 0,
-            min_id = 0,
-            hash = 0,
-        ))
 
-        if not history.messages:
+        history = await fetch_messages(client, my_channel, offset_id, limit)
+        if not await process_messages(history.messages, total_messages, total_count_limit, all_messages):
             break
-        messages = history.messages
-        for message in messages:
-            all_messages.append(message.to_dict())
-            offset_id = messages[len(messages)-1].id
-            total_messages = len(all_messages)
-            if total_count_limit != 0 and total_messages >= total_count_limit:
-                break
+
     return all_messages
 
-
-                                 
+                               
 async def store_messages(messages,filename = "telegram_messages.json"):
     async with aiofiles.open(filename,"w") as outfile:
         await json.dump(messages, outfile, cls = DateTimeEncoder)
