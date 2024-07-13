ca5704bc446ede5f99475a43ed833660418bd48a
NitionVR <kmaganyane83@gmail.com>
2024-07-13 22:44:29 +0200

    Add a test case for config_file() which loads the config file

---
+++ client/extract_links.py
@@ -0,0 +1,21 @@
+import re
+
+def read_file(filename="telegram_messages.json"):
+    try:
+        with open (filename,'r') as file:
+            text = file.read()
+        return text
+    except FileNotFoundError as err:
+        print(err)
+
+def extract_links(text):
+    pattern = re.compile(r'https://www\.youtube\.com/[\w@]+/videos')
+    youtube_links = pattern.findall(text)
+    return youtube_links
+
+
+if __name__ == "__main__":
+    text = read_file()
+    youtube_links = extract_links(text)
+
+    print(*youtube_links,sep="\n")
--- tests/telegram_client/test_telegram_client.py
+++ tests/telegram_client/test_telegram_client.py
@@ -1,9 +1,14 @@
+import configparser
 import unittest
+from client.telegram_client import config_file
 
 class TestTelegramClient(unittest.TestCase):
     
+    #Test that the config file is read as expected by ConfigParser
     def test_config_file(self):
-        pass
+        config = config_file()
+        self.assertIsInstance(config, configparser.ConfigParser)
+        self.assertEqual(config.section(), ['Telegram'])
 
 if __name__  == "__main__":
     unittest.main()
